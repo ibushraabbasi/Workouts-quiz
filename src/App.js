404b60c73ee5d91f2ba89ba3c6d40dbeb9eecb62
Bushra <bushraabbasi31@gmail.com>
2022-09-02 19:34:59 +0500

    Adding all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [showFinalResults, setFinalResult] = useState(false);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <div className='question-card'> {/* Question No*/}
+      <h5>Question 1 0f 5</h5>
+      
+      {/* Questions*/}
+      <h3 className='question-text'>Which types of workouts are your favorites?</h3>
+      <ul>
+        <li>Strength </li>
+        <li>Yoga</li>
+        <li>Meditation</li>
+        <li>Strength</li>
+      </ul>
+   
+      {/* Button Next and back */}
+      <button>Back</button>
+      <button>Next</button>
+      {/* Final page  */}
+       <div className='final-result'>
+           <h4>Top workouts for you</h4>
+           <p>Based on your answers, here’s a list of classes we think might interest you.</p>
+          
+           <span><button>Restart Quiz</button></span>
+           <span><button>Try Classes on the App</button></span>
+           </div>
+    </div> 
+   </div>
   );
 }
 

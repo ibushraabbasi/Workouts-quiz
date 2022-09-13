61ed2b183445cad9dd58c222fc45e342290645b2
Bushra <bushraabbasi31@gmail.com>
2022-09-13 18:04:11 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -88,109 +88,115 @@ function App() {
 
   // properties
   const [showFinalResults, setFinalResult] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(1);
   const [isActive, setIsActive] = useState(false);
 
-  const questions = [{
-         text: "Which types of workouts are your favorites?",
-         options: [
-              {id: 0, text: "Strength"},
-              {id: 1, text: "Yoga"},
-              {id: 2, text: "Meditation"},
-              {id: 3, text: "Stretching"}
-          ],
-      },
-      {
-          text: "What’s your experience level?",
-          options: [
-              {id: 0, text: "Beginner"},
-              {id: 1, text: "Intermediate"},
-              {id: 2, text: "Advanced"}
-          ],
-      },
-      {
-          text: "How long are your workouts?",
-          options: [
-              { id: 0, text: "5-20 mins"},
-              { id: 1, text: "20-30 mins"},
-              { id: 2, text: "30-40 mins"}
-          ],
-      },
-      {
-          text: "What workout accessories do you have available to use?",
-          options: [
-              {id: 0, text: "Light dumbbells"},
-              {id: 1, text: "Medium dumbbells"},
-              {id: 2, text: "Heavy dumbbells"},
-              {id: 3, text: "Resistance bands"},
-              {id: 4, text: "Workout mats"},
-              {id: 5, text: "Yoga mats"},
-              {id: 6, text: "Yoga blocks"},
-              {id: 7, text: "Foam roller"},
-              {id: 8, text: "None"}
-          ],
-      },
-    ]
+  const questions = [
+    {
+      text: "Which types of workouts are your favorites?",
+      options: [
+        {id: 0, text: "Strength"},
+        {id: 1, text: "Yoga"},
+        {id: 2, text: "Meditation"},
+        {id: 3, text: "Stretching"}
+      ],
+    },
+    {
+      text: "What’s your experience level?",
+      options: [
+        {id: 0, text: "Beginner"},
+        {id: 1, text: "Intermediate"},
+        {id: 2, text: "Advanced"}
+      ],
+    },
+    {
+      text: "How long are your workouts?",
+      options: [
+        { id: 0, text: "5-20 mins"},
+        { id: 1, text: "20-30 mins"},
+        { id: 2, text: "30-40 mins"}
+      ],
+    },
+    {
+      text: "What workout accessories do you have available to use?",
+      options: [
+        {id: 0, text: "Light dumbbells"},
+        {id: 1, text: "Medium dumbbells"},
+        {id: 2, text: "Heavy dumbbells"},
+        {id: 3, text: "Resistance bands"},
+        {id: 4, text: "Workout mats"},
+        {id: 5, text: "Yoga mats"},
+        {id: 6, text: "Yoga blocks"},
+        {id: 7, text: "Foam roller"},
+        {id: 8, text: "None"}
+      ],
+    },
+  ]
+
   const handleAnswerButtonClick = () => {
-     const nextQuestion = currentQuestion + 1;
-      if(nextQuestion < questions.length) {
-        setCurrentQuestion(nextQuestion);
-      } else{
-        setFinalResult(true);
-      }
+    setCurrentQuestion(currentQuestion + 1);
   };
+
   const handlebackButtonClick = () => {
-    const nextQuestion = currentQuestion - 1;
-     if(nextQuestion < questions.length) {
-       setCurrentQuestion(nextQuestion);
-     } else{
-       setFinalResult(true);
-     }
- };
+      setCurrentQuestion(currentQuestion - 1);
+  };
+
   const restartQuiz = () => {
-      setCurrentQuestion(0);
-      setFinalResult(false);
+    setCurrentQuestion(1);
+    setFinalResult(false);
   }
+
   const handleClick = () => {
     setIsActive(current => !current);
   }
-  return (
-      <div className="App">
-        {showFinalResults ? (
-        
-          /* Final page  */
-         <div className='final-result'>
-             <h4>Top workouts for you</h4>
-             <p>Based on your answers, here’s a list of classes we think might interest you.</p>
-            <span><button onClick={() => restartQuiz()}>Restart Quiz</button></span>
-            
-             </div>
-     ) : (
-        <div className='question-card'> 
-  
-        {/* Question No*/}
-        <h5>Question {currentQuestion + 1 } 0f {questions.length}</h5>
-          {/* Questions*/}
-       <h3 className='question-text'>{questions[currentQuestion].text}</h3>
-       <div>
-       <ul>
-         {questions[currentQuestion].options.map((option) => {
-          return (
-           <li style={{
-            backgroundColor: isActive ? 'salmon' : '',
-            color: isActive ? 'white' : '',
-          }}
-           onClick={handleClick}
-           key={option.id}>{option.text}</li>
-        );
-        })}
-       </ul></div>
-     {/* Button Next and back */}
 
-        <button onClick={() => handlebackButtonClick()}>Back</button>
-       <button onClick={() => handleAnswerButtonClick()}>Next</button>
-      </div> 
-    )}
+  const onFinsh = () =>{
+    setFinalResult(true);
+    
+  }
+
+  return (
+    <div className="App">
+      {showFinalResults ? 
+        <div className='final-result'>
+          <h4>Top workouts for you</h4>
+          <p>Based on your answers, here's a list of classes we think might interest you.</p>
+          <span><button onClick={() => restartQuiz()}>Restart Quiz</button></span>
+            
+        </div>
+      :
+        <div className='question-card'> 
+
+          <h5>Question {currentQuestion} of {questions.length}</h5>
+
+          <h3 className='question-text'>{questions[currentQuestion-1].text}</h3>         
+          <div>
+              {questions[currentQuestion-1].options.map((option) => {
+                return (
+                  <li style={{
+                    backgroundColor: isActive ? 'salmon' : '',
+                    color: isActive ? 'white' : '',
+                  }}
+                  onClick={handleClick}
+                  key={option.id}>{option.text}</li>
+                );
+              })}
+               </div>
+
+          <button 
+            onClick = {handlebackButtonClick}
+            disabled={currentQuestion===1?true:false}
+          >
+            Back
+          </button>
+
+          {currentQuestion===questions.length?
+            <button onClick={() => onFinsh()}>Finsh</button>
+            :
+            <button onClick={() => handleAnswerButtonClick()}>Next</button>
+          }
+        </div> 
+      }
     </div>
     );
   }

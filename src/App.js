e186011d067db1ce1077dd97365f9b715819c4e4
Bushra <bushraabbasi31@gmail.com>
2022-11-07 01:07:22 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable default-case */
+/* eslint-disable jsx-a11y/iframe-has-title */
 import { useEffect, useState } from "react";
 import "./App.css";
-import  {useFetch}  from '../src/hooks/useFetch'
+// import  {useFetch}  from '../src/hooks/useFetch'
+import dbase from "../src/data/db.json"
 
 function App() {
   // properties
@@ -15,49 +18,50 @@ function App() {
   });
 
   // For db.json data
- const [url, setUrl] = useState('http://localhost:3000/list')
-const { data } = useFetch(url)
-
+//  const [url, setUrl] = useState('http://localhost:3000/list')
+  //  const { data } = useFetch(url)
+   const data = dbase.list
+    
   const questions = [
     {
       text: "Which types of workouts are your favorites?",
       options: [
-        { id: 0, text: "Indoor Walking" },
-        { id: 1, text: "Sculpting" },
-        { id: 2, text: "Meditation" },
-        { id: 3, text: "Stretching" },
-        { id: 4, text: "HIIT Cardio" },
-        { id: 5, text: "Tread Bootcamp" },
+        { id: 0, workout: "Indoor Walking" },
+        { id: 1, workout: "Sculpting" },
+        { id: 2, workout: "Meditation" },
+        { id: 3, workout: "Stretching" },
+        { id: 4, workout: "HIIT Cardio" },
+        { id: 5, workout: "Tread Bootcamp" },
       ],
     },
     {
       text: "What’s your experience level?",
       options: [
-        { id: 0, text: "Beginner" ,matched:false} ,
-        { id: 1, text: "Intermediate" ,matched:false},
-        { id: 2, text: "Advanced" ,matched:false},
+        { id: 0, experience: "Beginner" } ,
+        { id: 1, experience: "Intermediate"},
+        { id: 2, experience: "Advanced" },
       ],
     },
     {
       text: "How long are your workouts?",
       options: [
-        { id: 0, text: "5-20 mins" ,matched:false},
-        { id: 1, text: "20-30 mins" ,matched:false},
-        { id: 2, text: "30-40 mins" ,matched:false},
+        { id: 0, duration: "5-20 mins" },
+        { id: 1, duration: "20-30 mins" },
+        { id: 2, duration: "30-40 mins" },
       ],
     },
     {
       text: "What workout accessories do you have available to use?",
       options: [
-        { id: 0, text: "Light dumbbells" ,matched:false},
-        { id: 1, text: "Medium dumbbells" ,matched:false},
-        { id: 2, text: "Heavy dumbbells" ,matched:false},
-        { id: 3, text: "Resistance bands" ,matched:false},
-        { id: 4, text: "Workout mats" ,matched:false},
-        { id: 5, text: "Yoga mats" ,matched:false},
-        { id: 6, text: "Yoga blocks" ,matched:false},
-        { id: 7, text: "Foam roller" ,matched:false},
-        { id: 8, text: "None" ,matched:false},
+        { id: 0, accessories: "Light dumbbells"},
+        { id: 1, accessories: "Medium dumbbells" },
+        { id: 2, accessories: "Heavy dumbbells" },
+        { id: 3, accessories: "Resistance bands" },
+        { id: 4, accessories: "Workout mats" },
+        { id: 5, accessories: "Yoga mats" },
+        { id: 6, accessories: "Yoga blocks" },
+        { id: 7, accessories: "Foam roller" },
+        { id: 8, accessories: "None" },
       ],
     },
   ];
@@ -96,22 +100,15 @@ const { data } = useFetch(url)
   }
 //compare four selected cards
 useEffect(() => {
-    if(readyData && url){
-    if(readyData.text === data){
-      setFinalResult(prevState => {
-        return prevState.map(state =>{
-          if(state.text === data){
-             return {...state, matched:true}
-          } else {
-            return state
-          }
-        })
-      })
-      
+    if(readyData && data){
+      if(JSON.stringify(readyData.workout) === JSON.stringify(data.workout)){
+        console.log("match")
+      } else{
+        console.log("not matched")
+      }
     } 
-    }
   },[readyData, data])
-console.log(data)
+  console.log(readyData, data)
 
   const handleAnswerButtonClick = () => {
     setCurrentQuestion(currentQuestion + 1);
@@ -146,8 +143,8 @@ return (
             <ul>
               {data.map(urrl =>(
                 <li key={urrl.id}>
-                  <p></p>
-                  <iframe  X-frame-Options = 'same orgins' src={urrl.videos}/>
+                  {/* <p>{found.videos}</p> */}
+                  <iframe src={urrl.videos}/>
                 </li>
               ))}
             </ul>
@@ -175,7 +172,10 @@ return (
                   }}
                     key={option.id}
                 >
-                  {option.text}
+                  {option.workout}
+                  {option.experience}
+                  {option.duration}
+                  {option.accessories}
                 </button>
               );
             })}
